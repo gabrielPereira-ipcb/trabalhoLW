04fa57acc3ddb11926d99a8b7fe1bd70df7f99fe
Gabriel de Santana Pereira <gsantana99.gs@gmail.com>
2024-03-26 15:22:17 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//pagina inicial
+/*o visitante: pode consultar os dados da empresa (localização, horários de
+funcionamento, preços, etc); registar-se como cliente;*/
+// o que é suposto ter nesta pagina??
+	//consultar dados
+	//registar-se como cliente
+		//ser um link para um pagina de login ou fazer isso na pagina?
+
+	echo "hello world";
+?>
